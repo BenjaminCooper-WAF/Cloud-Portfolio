0cd779644b4cefae31035181a03a889f5866417a
BenjaminCooper-WAF <nolimiternow@gmail.com>
2026-07-14 01:47:26 +0100

    first commit

--- main.tf
+++ main.tf
@@ -207,7 +207,7 @@ data "aws_iam_openid_connect_provider" "github_existing" {
 }
 
 locals {
-  github_oidc_arn = var.create_github_oidc_provider     ? aws_iam_openid_connect_provider.github[0].arn     : data.aws_iam_openid_connect_provider.github_existing[0].arn
+  github_oidc_arn = var.create_github_oidc_provider ? aws_iam_openid_connect_provider.github[0].arn : data.aws_iam_openid_connect_provider.github_existing[0].arn
 }
 
 data "aws_iam_policy_document" "github_assume_role" {
